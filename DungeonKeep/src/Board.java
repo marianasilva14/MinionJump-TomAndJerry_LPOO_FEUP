d84ce5b21e5373147d92d61dbdd32a504ab13016
Mariana Silva <up201506197@fe.up.pt>
2017-02-17 12:50:21 +0000

    Correct board

--- DungeonKeep/src/Board.java
+++ DungeonKeep/src/Board.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
 
 public class Board {
 	
 	char[][] board = {{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
-			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','H',' ',' ','I',' ','X',' ','G','X'},
+			{'X','X','X','X','X','X','X',' ',' ','X'},
+			{'X',' ','I',' ','I',' ','X',' ',' ','X'},
+			{'X','X','X',' ','X','X','X',' ',' ','X'},
+			{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X','X','X',' ','X','X','X','X',' ','X'},
+			{'X',' ','I',' ','I',' ','X','k',' ','X'},
 			{'X','X','X','X','X','X','X','X','X','X'}};
 	
-	public void Play()
+	int row=1;
+	int col=1;
+	
+	public void printBoard()
 	{
 		int row = board.length;
 		int col = board[0].length;
@@ -26,8 +30,60 @@ public class Board {
 			
 			System.out.println();
 		}
-		
 	}
+	
+	public void Play()
+	{
+		printBoard();
+		
+		System.out.println();
+		System.out.println("Please insert a character.");
+		System.out.println("To move down, insert 'd'");
+		System.out.println("To move up, insert 'u'");
+		System.out.println("To move left, insert 'l'");
+		System.out.println("To move right, insert 'r'");
+		
+		Scanner reader = new Scanner(System.in);
+		char c = reader.next().charAt(0);
+		
+		 switch (c) {
+         case 'u': movementUp();
+                  break;
+         case 'd': movementDown();
+                  break;
+         case 'l': movementLeft();
+         		break;
+         case 'r': movementRight();
+  					break;
+         default: System.out.println("Invalid character");
+                  break;
+		 }
+	}
+	
+	public void movementUp(){
+		
+		board[row][col]= ' ';
+		board[row-1][col]='H';
+		
+		printBoard();
+	
+	}
+	
+	public void movementDown(){
+		
+		row +=1;
+	}
+	
+	public void movementLeft(){
+		
+		col -=1;
+	}
+	
+	public void movementRight(){
+		col +=1;
+	}
+	
+	
 	
 	public static void main(String[] args) {	
 				
