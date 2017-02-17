de447ba45298497e74d0ec2c1c2d2f1a4209c4df
Mariana Silva <up201506197@fe.up.pt>
2017-02-17 11:14:20 +0000

    First commit

---
+++ DungeonKeep/src/Board.java
@@ -0,0 +1,27 @@
+
+public class Board {
+
+	
+	char[][] board = {{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'},
+					{'X','X','X','X','X','X','X','X','X','X'}};
+	
+	public void Play()
+	{
+		System.out.println("fhsggd");
+	}
+	
+	public static void main(String[] args) {	
+
+
+		new Board().Play();
+	}
+
+}
