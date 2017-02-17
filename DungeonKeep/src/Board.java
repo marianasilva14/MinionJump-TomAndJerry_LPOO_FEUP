0a9ba1aa194c7821e74a3f48dbb61c7e0aace48b
Mariana Silva <up201506197@fe.up.pt>
2017-02-17 11:44:13 +0000

    Print out the following map (no colors, textual characters only)

--- DungeonKeep/src/Board.java
+++ DungeonKeep/src/Board.java
@@ -1,27 +1,37 @@
 
 public class Board {
-
 	
 	char[][] board = {{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'},
-					{'X','X','X','X','X','X','X','X','X','X'}};
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'X','X','X','X','X','X','X','X','X','X'}};
 	
 	public void Play()
 	{
-		System.out.println("fhsggd");
+		int row = board.length;
+		int col = board[0].length;
+		
+		for(int i = 0; i < row; i++)
+		{
+			for(int j = 0; j < col; j++)
+			{
+				System.out.print(board[i][j]+" ");
+			}
+			
+			System.out.println();
+		}
+		
 	}
 	
 	public static void main(String[] args) {	
-
-
-		new Board().Play();
+				
+		(new Board()).Play();
 	}
 
 }
