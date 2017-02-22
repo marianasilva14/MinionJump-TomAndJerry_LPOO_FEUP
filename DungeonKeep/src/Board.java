e13a8bb113d38508bb4c6ddfad08c15455839a4c
Mariana Silva <up201506197@fe.up.pt>
2017-02-22 16:23:37 +0000

    Task2 completed

--- DungeonKeep/src/Board.java
+++ DungeonKeep/src/Board.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Board {
-	
+
 	char[][] board = {{'X','X','X','X','X','X','X','X','X','X'},
 			{'X','H',' ',' ','I',' ','X',' ','G','X'},
-			{'X','X','X','X','X','X','X',' ',' ','X'},
+			{'X','X','X',' ','X','X','X',' ',' ','X'},
 			{'X',' ','I',' ','I',' ','X',' ',' ','X'},
 			{'X','X','X',' ','X','X','X',' ',' ','X'},
 			{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
@@ -12,81 +12,198 @@ public class Board {
 			{'X','X','X',' ','X','X','X','X',' ','X'},
 			{'X',' ','I',' ','I',' ','X','k',' ','X'},
 			{'X','X','X','X','X','X','X','X','X','X'}};
-	
-	int row=1;
-	int col=1;
-	
+
+	int row = 1;
+	int col = 1;
+
 	public void printBoard()
 	{
 		int row = board.length;
 		int col = board[0].length;
-		
+
 		for(int i = 0; i < row; i++)
 		{
 			for(int j = 0; j < col; j++)
 			{
 				System.out.print(board[i][j]+" ");
 			}
-			
+
 			System.out.println();
 		}
 	}
-	
+
+	public boolean checkGuard()
+	{
+		if(board[row-1][col] == 'G' || board[row+1][col] == 'G' || board[row][col-1] == 'G'|| board[row][col+1] == 'G')
+			return true;
+
+		return false;
+	}
+	/*
+	public void movementGuard()
+	{
+
+	}*/
+
+	public void checkLever(){
+		int row_aux = board.length;
+		int col_aux = board[0].length;
+
+		for(int i = 0; i < row_aux; i++)
+		{
+			for(int j = 0; j < col_aux; j++)
+			{
+				if(board[i][j] == 'I')
+					board[i][j] = 'S';
+			}
+		}
+
+		printBoard();
+
+	}
+
+
+	public void chooseOption()
+	{
+		if(checkGuard()) 
+			System.out.print("You got caught! Game Over!");
+		else{
+			System.out.println();
+			System.out.println("Please insert a character.");
+			System.out.println("To move down, insert 'd'");
+			System.out.println("To move up, insert 'u'");
+			System.out.println("To move left, insert 'l'");
+			System.out.println("To move right, insert 'r'");
+
+			Scanner reader = new Scanner(System.in);
+			char c = reader.next().charAt(0);
+
+			switch (c) {
+			case 'u': movementUp();
+			break;
+			case 'd': movementDown();
+			break;
+			case 'l': movementLeft();
+			break;
+			case 'r': movementRight();
+			break;
+			default: System.out.println("Invalid character");
+			break;
+			}
+		}
+	}
+
 	public void Play()
 	{
 		printBoard();
-		
-		System.out.println();
-		System.out.println("Please insert a character.");
-		System.out.println("To move down, insert 'd'");
-		System.out.println("To move up, insert 'u'");
-		System.out.println("To move left, insert 'l'");
-		System.out.println("To move right, insert 'r'");
-		
-		Scanner reader = new Scanner(System.in);
-		char c = reader.next().charAt(0);
-		
-		 switch (c) {
-         case 'u': movementUp();
-                  break;
-         case 'd': movementDown();
-                  break;
-         case 'l': movementLeft();
-         		break;
-         case 'r': movementRight();
-  					break;
-         default: System.out.println("Invalid character");
-                  break;
-		 }
+		chooseOption();
 	}
-	
+
 	public void movementUp(){
-		
-		board[row][col]= ' ';
-		board[row-1][col]='H';
-		
-		printBoard();
-	
+		boolean movement = false;
+
+		if(board[row-1][col] == ' ')
+		{
+			movement = true;
+			board[row][col]= ' ';
+			board[row-1][col]='H';
+			printBoard();
+			row=row-1;
+			chooseOption();
+		}
+		else if(board[row-1][col] == 'k'){
+			checkLever();
+			System.out.print("You managed to open the doors!");
+			chooseOption();
+		}
+		else if(board[row-1][col] == 'X'|| board[row-1][col] == 'I') 
+		{
+			System.out.print("Invalid movement! Try again");
+			chooseOption();
+		}
+
+
 	}
-	
-	public void movementDown(){
-		
-		row +=1;
+
+	public void movementDown(){	
+		boolean movement = false;
+
+		if(board[row+1][col] == ' ')
+		{
+			movement = true;
+			board[row][col]= ' ';
+			board[row+1][col]='H';
+			printBoard();
+			row=row+1;
+			chooseOption();
+		}
+		else if(board[row+1][col] == 'k'){
+			checkLever();
+			System.out.print("You managed to open the doors!");
+			chooseOption();
+		}
+		else if(board[row+1][col] == 'X'|| board[row+1][col] == 'I') 
+		{
+			System.out.print("Invalid movement! Try again");
+			chooseOption();
+		}
+
+
 	}
-	
+
 	public void movementLeft(){
-		
-		col -=1;
+		boolean movement = false;
+
+		if(board[row][col-1] == ' ')
+		{
+			movement = true;
+			board[row][col]= ' ';
+			board[row][col-1]='H';
+			printBoard();
+			col=col-1;
+			chooseOption();
+		}
+
+		else if(board[row][col-1] == 'k'){
+			checkLever();
+			System.out.print("You managed to open the doors!");
+			chooseOption();
+		}
+		else if(board[row][col-1] == 'X'|| board[row][col-1] == 'I') 
+		{
+			System.out.print("Invalid movement! Try again");
+			chooseOption();
+		}
+
 	}
-	
+
 	public void movementRight(){
-		col +=1;
+		boolean movement = false;
+
+		if(board[row][col+1] == ' ')
+		{
+			movement = true;
+			board[row][col]= ' ';
+			board[row][col+1]='H';
+			printBoard();
+			col=col+1;
+			chooseOption();
+		}
+
+		else if(board[row][col+1] == 'k'){
+			checkLever();
+			System.out.print("You managed to open the doors!");
+			chooseOption();
+		}
+		else if(board[row][col+1] == 'X'|| board[row][col+1] == 'I') 
+		{
+			System.out.print("Invalid movement! Try again");
+			chooseOption();
+		}
 	}
-	
-	
-	
+
 	public static void main(String[] args) {	
-				
+
 		(new Board()).Play();
 	}
 
