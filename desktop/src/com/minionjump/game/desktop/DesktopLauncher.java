38fa2b8296ac151caecab085496c9234d0041030
Mariana Silva <up201506197@fe.up.pt>
2017-05-05 12:23:06 +0100

    Fixed bugs

---
+++ desktop/src/com/minionjump/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.minionjump.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.minionjump.game.MyMinionJump;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyMinionJump(), config);
+	}
+}
