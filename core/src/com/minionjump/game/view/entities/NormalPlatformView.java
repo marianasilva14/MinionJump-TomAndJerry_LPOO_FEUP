b202848cd9a324b6d6152ae9c606d3541a1639d1
Mariana Silva <up201506197@fe.up.pt>
2017-05-06 12:52:04 +0100

    Added images and complete package view

--- core/src/com/minionjump/game/view/entities/NormalPlatformView.java
+++ core/src/com/minionjump/game/view/entities/NormalPlatformView.java
@@ -1,8 +1,22 @@
 package com.minionjump.game.view.entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.minionjump.game.MyMinionJump;
+
 /**
  * Created by Mariana on 05/05/2017.
  */
 
-public class NormalPlatformView {
+public class NormalPlatformView extends EntityView{
+
+    public NormalPlatformView(MyMinionJump game){
+        super(game);
+    }
+
+    public Sprite createSprite(MyMinionJump game){
+        Texture texture = game.getAssetManager().get("platform.png");
+
+        return new Sprite(texture, texture.getWidth(), texture.getHeight());
+    }
 }
--- core/src/com/minionjump/game/view/entities/VillainView.java
+++ core/src/com/minionjump/game/view/entities/VillainView.java
@@ -1,8 +1,22 @@
 package com.minionjump.game.view.entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.minionjump.game.MyMinionJump;
+
 /**
  * Created by Mariana on 05/05/2017.
  */
 
-public class VillainView {
+public class VillainView extends EntityView {
+
+    public VillainView(MyMinionJump game){
+        super(game);
+    }
+
+    public Sprite createSprite(MyMinionJump game){
+        Texture texture = game.getAssetManager().get("villain.png");
+
+        return new Sprite(texture, texture.getWidth(), texture.getHeight());
+    }
 }
