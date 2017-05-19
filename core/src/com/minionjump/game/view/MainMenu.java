974c155ca9024c8c9275135dd81e906725bc4b2a
Francisca Cerquinho <franciscacerquinho@hotmail.com>
2017-05-19 10:54:59 +0100

    Update

--- core/src/com/minionjump/game/view/MainMenu.java
+++ core/src/com/minionjump/game/view/MainMenu.java
@@ -2,14 +2,19 @@ package com.minionjump.game.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.minionjump.game.MyMinionJump;
@@ -18,12 +23,13 @@ import com.minionjump.game.MyMinionJump;
  * Created by Mariana on 08/05/2017.
  */
 
-public class MainMenu extends State{
+public class MainMenu extends State {
     private Texture background;
     private Texture playBtn;
     private Texture scoresBtn;
     private Texture optionsBtn;
-    private Image playButton,scoresButton, optionsButton;
+    private TextButton playButton,scoresButton, optionsButton;
+    private ImageButton testeBut;
     private PlayState play;
     private Stage stage;
     private MyMinionJump minionJump;
@@ -35,6 +41,7 @@ public class MainMenu extends State{
         playBtn = new Texture("btnplay.png");
         scoresBtn = new Texture("scores.png");
         optionsBtn = new Texture("options.png");
+        stage = new Stage();
 /*
         gamePort=new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         gamePort.apply();
@@ -53,37 +60,57 @@ public class MainMenu extends State{
         playButton.setPosition(xPlayButton,yPlayButton);
         Gdx.input.setInputProcessor(stage);
         stage.addActor(playButton);
+*/
 
-        playButton.addListener(new ClickListener(){
+        Drawable buttonDrawableSha = new TextureRegionDrawable(new TextureRegion(playBtn));
+        testeBut = new ImageButton(buttonDrawableSha);
+        testeBut.setSize(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/9);
+        testeBut.setPosition(Gdx.graphics.getWidth()/2-testeBut.getWidth()/2,Gdx.graphics.getHeight()/2-testeBut.getHeight()/2);
+
+
+
+        testeBut.addListener(new ClickListener(){
+            @Override
             public void clicked(InputEvent event, float x,float y){
-              gam.set(new PlayState(gam));
+
+               // gam.set(new PlayState(gam));
+                System.out.println("Button Pressed");
             }
-        });*/
+        });
+
+
+        stage.addActor(testeBut);
     }
 
     @Override
 
     public void handleInput() {
 
-       if(Gdx.input.justTouched()){
-            gam.set(new PlayState(gam));
-        }
+//       if(Gdx.input.justTouched()){
+//            gam.set(new PlayState(gam));
+//        }
+//        while(!testeBut.getClickListener().isPressed()){
+//
+//        }
 
     }
 
     @Override
     public void update(float dt) {
-        handleInput();
+       // handleInput();
     }
 
     @Override
     public void render(SpriteBatch sb) {
+
         sb.begin();
         sb.draw(background,0,0, MyMinionJump.WIDTH, MyMinionJump.HEIGHT);
         sb.draw(playBtn, (MyMinionJump.WIDTH/4)-(playBtn.getWidth()/3), MyMinionJump.HEIGHT/3);
         sb.draw(scoresBtn, (MyMinionJump.WIDTH) - (optionsBtn.getWidth()), scoresBtn.getHeight() - (scoresBtn.getHeight()-1));
         sb.draw(optionsBtn, (MyMinionJump.WIDTH/2) - (scoresBtn.getWidth()/5), (optionsBtn.getHeight()-5) - (optionsBtn.getHeight()));
         sb.end();
+        stage.act();
+        stage.draw();
     }
 
     @Override
